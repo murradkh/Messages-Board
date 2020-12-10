71648a23b2e63aca3ff063d69ff7114ae24d4698
murad <murradkhalil@gmail.com>
2020-12-10 05:00:09 -0800

    removing currency field

--- solution/user_management/models.py
+++ solution/user_management/models.py
@@ -6,5 +6,5 @@ from currency_management.models import Currency
 
 class Profile(models.Model):
     usr = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-    favorite_currency = models.ForeignKey(Currency, related_name="profiles", on_delete=models.SET_NULL, null=True)
+    # favorite_currency = models.ForeignKey(Currency, related_name="profiles", on_delete=models.SET_NULL, null=True)
     img = models.FilePathField(path="user_management/static/images")
--- solution/user_management/views.py
+++ solution/user_management/views.py
@@ -4,29 +4,29 @@ from django.shortcuts import redirect, render
 from django.urls import reverse
 from .forms import UserForm
 from .models import Profile
-from currency_management.forms import CurrencyForm
-from currency_management.models import Currency
+# from currency_management.forms import CurrencyForm
+# from currency_management.models import Currency
 
 
 def register(request):
     if request.method == "GET":
         return render(
             request, "registration/register.html",
-            {"user_form": UserForm, "currency_form": CurrencyForm}
+            {"user_form": UserForm}
         )
     elif request.method == "POST":
-        currency_form = CurrencyForm(request.POST)
+        # currency_form = CurrencyForm(request.POST)
         user_form = UserForm(request.POST)
-        if currency_form.is_valid() and user_form.is_valid():
+        if user_form.is_valid():
             user = user_form.save(commit=False)
             user.password = make_password(user.password)
             user.save()
-            currency = Currency.objects.get(currency_name=currency_form.cleaned_data.get("currency_name"))
-            Profile(usr=user, favorite_currency=currency).save()
+            # currency = Currency.objects.get(currency_name=currency_form.cleaned_data.get("currency_name"))
+            Profile(usr=user).save()
             return render(request, "registration/success_registration.html")
         else:
             return render(request, "registration/register.html",
-                          {"user_form": user_form, "currency_form": currency_form,
-                           "errors": {**user_form.errors, **currency_form.errors}})
+                          {"user_form": user_form,
+                           "errors": {**user_form.errors}})
 
     return redirect(reverse("home"))
